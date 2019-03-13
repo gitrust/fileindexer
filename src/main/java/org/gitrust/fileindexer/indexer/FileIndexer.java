dbbd8942b39cfa9dbf68eebc8aa12f7512b1f7d4
gitrust <weebac@web.de>
2019-03-13 09:54:13 +0100

    First example of writing a document

--- src/main/java/org/gitrust/fileindexer/indexer/FileIndexer.java
+++ src/main/java/org/gitrust/fileindexer/indexer/FileIndexer.java
@@ -1,30 +1,47 @@
 package org.gitrust.fileindexer.indexer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexWriter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileIndexer {
+    static Logger LOG = LogManager.getLogger(FileIndexer.class);
 
     private static final String INDEX_DIR = "c:/temp/lucene6index";
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage: fileindexer <directory>");
+            System.exit(1);
+        }
+        String filePath = args[1];
         IndexWriter writer = IndexWriterFactory.createWriter(INDEX_DIR);
-        List<Document> documents = new ArrayList<>();
 
-        FileDocument fd = FileDocument.builder().absolutePath("").fileName("").fileSize(0).build();
-        Document document1 = DocumentFactory.createDocument(fd);
-        documents.add(document1);
-
-        //Let's clean everything first
         writer.deleteAll();
 
-        writer.addDocuments(documents);
+        Files.walk(Paths.get(filePath)).filter(Files::isRegularFile).forEach(path -> writeDocument(writer, path));
+
         writer.commit();
         writer.close();
+    }
+
+    private static void writeDocument(IndexWriter writer, Path path) {
+        LOG.debug("Index path {}", path);
+        File file = path.toFile();
+        FileDocument fd = FileDocument.builder().absolutePath(file.getAbsolutePath()).fileName(file.getName()).fileSize(file.getTotalSpace()).build();
+        Document doc = DocumentFactory.createDocument(fd);
+        try {
+            writer.addDocument(doc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
